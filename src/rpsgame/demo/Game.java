14deeb5aeecd0025e43e2ba55ccb13c31a8bc8aa
qwang <qwang@xmltravelgate.com>
2020-10-23 01:01:26 +0200

    fixed Game Class

--- src/rpsgame/demo/Game.java
+++ src/rpsgame/demo/Game.java
@@ -1,105 +1,52 @@
-//package rpsgame.demo;
-//
-//import java.io.IOException;
-//import java.util.concurrent.BlockingQueue;
-//import java.util.concurrent.LinkedBlockingQueue;
-//import java.util.concurrent.atomic.AtomicReference;
-//import rpsgame.server.MyHttpHandler;
-//import rpsgame.server.MyHttpServer;
-//import rpsgame.common.CommonUtils;
-//
-//public class Game {
-//
-//  private final Player p1;
-//
-//  private final Player p2;
-//
-//  private final Play[] plays;
-//
-//  /**
-//   * Constructor.
-//   *
-//   * @param p1        {@link Player} first player.
-//   * @param p2        {@link Player} second player.
-//   * @param iteration {@code int} number of iterations.
-//   */
-//  public Game(Player p1, Player p2, int iteration) {
-//    this.p1 = p1;
-//    this.p2 = p2;
-//    this.plays = new Play[iteration];
-//    startPlays(iteration);
-//  }
-//
-//
-//  private void startPlays(int iteration) {
-//
-//    for (int x = 0; x < iteration; x++) {
-//      Play play = new Play(x + 1, this.p1, this.p2, this.p1.getRdmChoice(), this.p2.getRdmChoice());
-//      this.plays[x] = play;
-//    }
-//  }
-//
-//  private void standalonePlay(int iteration) {
-//    for (int x = 0; x < iteration; x++) {
-//      Play play = new Play(x + 1, this.p1, this.p2, this.p1.getRdmChoice(), this.p2.getRdmChoice());
-//      this.plays[x] = play;
-//    }
-//  }
-//
-//  private void remotePlay(int iteration) throws IOException, InterruptedException {
-//    boolean isFinished = false;
-//    int idx = 0;
-//    // Server set up
-//    BlockingQueue<String> queue = new LinkedBlockingQueue<>(1);
-//    MyHttpHandler httpHandler = new MyHttpHandler(queue);
-//    MyHttpServer httpServer = new MyHttpServer(httpHandler);
-//    httpServer.start();
-//
-//    // Active waiting
-//    for (int x = 0; x < iteration; x++) {
-//      System.out.println("Game ["+x+1+"] waiting for remote player's choice");
-//      var response = new AtomicReference<>("");
-//      while (response.get().isBlank()) {
-//        response.set(queue.take());
-//        //  Thread.sleep(500);
-//      }
-//
-//      var responseMap = CommonUtils.getQueryMap(response.get());
-//      Play play = new Play(x + 1, this.p1, this.p2, this.p1.getRdmChoice(), responseMap.get("player"));
-//      this.plays[x] = play;
-//    }
-//
-//  }
-//
-////  private boolean anyRemotePlayer(){
-////    return this.p1.getIsRemote() || this.p2.getIsRemote();
-////  }
-//
-//  private Player getRemotePlayer() {
-//    if (this.p1.getIsRemote()) {
-//      return this.p1;
-//    } else if (this.p2.getIsRemote()) {
-//      return this.p2;
-//    }
-//    return null;
-//  }
-//
-//
-//  @Override
-//  public String toString() {
-//    String msg = "";
-//    for (Play play : this.plays) {
-//      msg = msg.concat(play.toString());
-//    }
-//    msg = msg.concat(finalResultInfo(this.p1, this.p2));
-//    return msg;
-//  }
-//
-//  private String finalResultInfo(Player p1, Player p2) {
-//    return "\n\n===================\n FINAL RESULT \n===================\n"
-//        + p1.toString() + "\n" + p2.toString();
-//  }
-//
-//}
-//
-//
+package rpsgame.demo;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Game {
+
+  private final Player p1;
+
+  private final Player p2;
+
+  private final Play[] plays;
+
+  /**
+   * Constructor.
+   *
+   * @param p1                {@link Player} first player.
+   * @param p2                {@link Player} second player.
+   * @param iteration         {@code int} number of iterations.
+   * @param remoteChoiceQueue {@link BlockingQueue} queue for receive remote Player's choice.
+   */
+  public Game(Player p1, Player p2, int iteration, BlockingQueue<String> remoteChoiceQueue) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.plays = new Play[iteration];
+    startPlays(iteration, remoteChoiceQueue);
+  }
+
+  private void startPlays(int iteration, BlockingQueue<String> queue) {
+    for (int x = 0; x < iteration; x++) {
+      Play play = new Play(x + 1, this.p1, this.p2, queue);
+      this.plays[x] = play;
+    }
+  }
+
+  @Override
+  public String toString() {
+    String msg = "";
+    for (Play play : this.plays) {
+      msg = msg.concat(play.toString());
+    }
+    msg = msg.concat(finalResultInfo(this.p1, this.p2));
+    return msg;
+  }
+
+  private String finalResultInfo(Player p1, Player p2) {
+    return "\n\n===================\n FINAL RESULT \n===================\n"
+        + p1.toString() + "\n" + p2.toString();
+  }
+
+}
+
+
--- src/rpsgame/test/GameTest.java
+++ src/rpsgame/test/GameTest.java
@@ -1,31 +1,79 @@
-//package rpsgame.test;
-//
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//import rpsgame.demo.Game;
-//import rpsgame.demo.Player;
-//
-//public class GameTest {
-//
-//  @Test
-//  public void gameTest() {
-//    // Arrange
-//    int iteration = 10;
-//    Player p1 = new Player("player1", true);
-//    Player p2 = new Player("player2", true);
-//
-//    // Act
-//    Game game = new Game(p1, p2, iteration);
-//
-//    // Asserts
-//
-//    // result is not empty
-//    Assert.assertFalse(game.toString().isEmpty());
-//
-//    // the sum of p1 win's counter, p2 win's counter
-//    // and drawCounter of one of them must be the number of iterations.
-//    Assert.assertEquals(p1.getWinCounter() + p2.getWinCounter() + p1.getDrawCounter(), iteration);
-//    Assert.assertEquals(p1.getWinCounter() + p2.getWinCounter() + p2.getDrawCounter(), iteration);
-//  }
-//}
+package rpsgame.test;
+
+
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import org.junit.Assert;
+import org.junit.Test;
+import rpsgame.common.CommonUtils;
+import rpsgame.demo.Game;
+import rpsgame.demo.Player;
+import rpsgame.server.MyHttpHandler;
+import rpsgame.server.MyHttpServer;
+
+public class GameTest {
+
+
+  @Test
+  public void gameTest() {
+    // Arrange
+    int iteration = 10;
+    Player p1 = new Player("player1", false, true);
+    Player p2 = new Player("player2", false, true);
+
+    // Act
+    Game game = new Game(p1, p2, iteration, null);
+
+    // Asserts
+    // result is not empty
+    Assert.assertFalse(game.toString().isEmpty());
+
+    // the sum of p1 win's counter, p2 win's counter
+    // and drawCounter of one of them must be the number of iterations.
+    Assert.assertEquals(p1.getWinCounter() + p2.getWinCounter() + p1.getDrawCounter(), iteration);
+    Assert.assertEquals(p1.getWinCounter() + p2.getWinCounter() + p2.getDrawCounter(), iteration);
+  }
+
+
+  @Test
+  public void remoteGameTest() throws IOException {
+
+    // Server set up
+    int iteration = 10;
+    BlockingQueue<String> queue = new LinkedBlockingQueue<>(1);
+    MyHttpHandler httpHandler = new MyHttpHandler(queue);
+    MyHttpServer httpServer = new MyHttpServer(httpHandler);
+    httpServer.start();
+    // Act
+    new Thread(() -> remoteGameProcess(iteration, queue)).start();
+    new Thread(() -> httpCallProcess(iteration)).start();
+  }
+
+
+  private void remoteGameProcess(int iteration, BlockingQueue<String> queue) {
+    // Arrange
+    Player p1 = new Player("player1", false, true);
+    Player p2 = new Player("player2", true, true);
+
+    // Act
+    Game game = new Game(p1, p2, iteration, queue);
+
+    // Asserts
+    // result is not empty
+    Assert.assertFalse(game.toString().isEmpty());
+
+    // the sum of p1 win's counter, p2 win's counter
+    // and drawCounter of one of them must be the number of iterations.
+    Assert.assertEquals(p1.getWinCounter() + p2.getWinCounter() + p1.getDrawCounter(), iteration);
+    Assert.assertEquals(p1.getWinCounter() + p2.getWinCounter() + p2.getDrawCounter(), iteration);
+  }
+
+  private void httpCallProcess(int iteration) {
+    String url = "http://localhost:8081/myserver?choice=Rock";
+    for (int x = 0; x < iteration; x++) {
+      CommonUtils.sendHttpCall(url, "POST");
+    }
+  }
+
+}
